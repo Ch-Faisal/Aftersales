c01f7704796d94dadfd577d2e284b2e028e603e1
Ahsan Ali <ahsan.ali@otif-solutions.com>
2024-03-14 16:59:33 +0500

    heaader and footer added as a seprate files

---
+++ src/pages/layout/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+        <div className="bg-white container-fluid">
+            <div className='container text-center mt-3'>
+                <p className='fw-bold'>©2023 Toyota Astra Motor</p>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
